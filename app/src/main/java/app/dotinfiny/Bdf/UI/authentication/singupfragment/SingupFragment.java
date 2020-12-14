86f4fb4cdf0f942c6175c229ecf1612b1969a458
saiif12 <saif_khan09@hotmail.com>
2020-12-14 11:43:40 +0500

    signup completed

--- app/src/main/java/app/dotinfiny/Bdf/UI/authentication/singupfragment/SingupFragment.java
+++ app/src/main/java/app/dotinfiny/Bdf/UI/authentication/singupfragment/SingupFragment.java
@@ -2,16 +2,21 @@ package app.dotinfiny.Bdf.UI.authentication.singupfragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import app.dotinfiny.Bdf.R;
 
 
 public class SingupFragment extends Fragment {
+
+    EditText EtUserName,EtEmail,EtPassword,EtPhone;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,4 +30,27 @@ public class SingupFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_singup, container, false);
     }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(view);
+        oncreate();
+    }
+
+
+    private void oncreate() {
+    }
+
+
+    private void init(View view) {
+        EtUserName = view.findViewById(R.id.ed_name_singup);
+        EtEmail = view.findViewById(R.id.edEmail_singup);
+        EtPhone = view.findViewById(R.id.etNumber_singup);
+        EtPassword = view.findViewById(R.id.etPassword_singup);
+
+
+    }
+
 }
